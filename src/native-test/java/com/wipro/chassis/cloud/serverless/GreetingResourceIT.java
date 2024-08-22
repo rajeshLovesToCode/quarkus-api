d068b25cc83429cf260957e6a334b6cf08a11026
quarkusio <no-reply@quarkus.io>
2024-08-22 12:45:00 +0000

    Initial commit

---
+++ src/native-test/java/com/wipro/chassis/cloud/serverless/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.wipro.chassis.cloud.serverless;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
